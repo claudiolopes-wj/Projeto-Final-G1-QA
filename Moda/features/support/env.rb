5d44189565b75536e7f370c1f43d1e6059f67a6d
davidwebjump <102738944+davidwebjump@users.noreply.github.com>
2022-05-12 09:12:49 -0300

    Revert "Setando a url padrão no arq env no projeto Moda"

--- Moda/features/support/env.rb
+++ Moda/features/support/env.rb
@@ -3,7 +3,7 @@ require 'capybara/cucumber'
 require 'report_builder'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.default_max_wait_time=10
-    config.app_host = "http://moda.magento.local/"
+    config.app_host = "https://magento.nublue.co.uk/" #site que vai ser aberto durante os teste
 end
