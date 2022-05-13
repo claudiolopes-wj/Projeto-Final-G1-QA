b9fb9ee82ff814b04b289412a0caa7ebf19a9b0a
David Lima <david.lima@webjump.com.br>
2022-05-13 12:53:16 -0300

    Realizado a automação na tela de cadastro.

---
+++ Moda/features/step_definitions/Cadastro/cadastro_invalido.rb
@@ -0,0 +1,10 @@
+
+createAccount = CreateAccountPage.new
+
+Dado("preencho todos os campos {string},{string}, {string}, {string} e {string}") do |nome, sobrenome, email, senha, senhaConfirm|
+    createAccount.Fill(nome, sobrenome, email, senha, senhaConfirm, false)
+end
+
+Entao("irei visualizar uma mensagem de erro {string}") do |mensagem|
+    createAccount.Assert(mensagem)
+end
